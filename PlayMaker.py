c2e37d2fe5061fab4ca1fcbdb5dad495a9054214
l-mayorga <lmayorga@bowdoin.edu>
2024-09-18 20:13:12 -0400

    [Buggy] Re-introduce transition matrix. Some transitions are not animating correctly. Likely due to cut logic in game loop.

--- PlayMaker.py
+++ PlayMaker.py
@@ -2,7 +2,7 @@ import numpy as np
 import pygame
 
 SCREEN_WIDTH = 800
-SCREEN_HIGHT = 700
+SCREEN_HEIGHT = 700
 
 # Define field dimensions
 FIELD_WIDTH = 400
@@ -21,71 +21,55 @@ TRANSITION_MATRIX = {
     "START": {
         "START": 0.0,
         "RESET_BREAK": 0.25,
-        "RESEST_UPLINE": 0.15,
+        "RESET_UPLINE": 0.15,
         "STACK_IN_CUT": 0.45,
         "STACK_BREAK_CUT": 0.05,
-        "STACK_DEEP_CUT": 0.05,
-        "HAIL_MARY": 0.05,
+        "STACK_DEEP_CUT": 0.1,
     },
 
     "RESET_BREAK": {
         "START": 0.0,
         "RESET_BREAK": 0.0,
-        "RESEST_UPLINE": 1.0,
+        "RESET_UPLINE": 1.0,
         "STACK_IN_CUT": 0.0,
         "STACK_BREAK_CUT": 0.0,
         "STACK_DEEP_CUT": 0.0,
-        "HAIL_MARY": 0.1,
     },
 
-    "RESEST_UPLINE": {
+    "RESET_UPLINE": {
         "START": 0.0,
         "RESET_BREAK": 0.0,
-        "RESEST_UPLINE": 0.0,
-        "STACK_IN_CUT": 0.1,
+        "RESET_UPLINE": 0.0,
+        "STACK_IN_CUT": 0.2,
         "STACK_BREAK_CUT": 0.0,
-        "STACK_DEEP_CUT": 0.7,
-        "HAIL_MARY": 0.2,
+        "STACK_DEEP_CUT": 0.8,
     },
 
     "STACK_IN_CUT": {
         "START": 0.0,
         "RESET_BREAK": 0.4,
-        "RESEST_UPLINE": 0.2,
+        "RESET_UPLINE": 0.2,
         "STACK_IN_CUT": 0.2,
         "STACK_BREAK_CUT": 0.1,
         "STACK_DEEP_CUT": 0.1,
-        "HAIL_MARY": 0.0,
     },
 
     "STACK_BREAK_CUT": {
         "START": 0.0,
         "RESET_BREAK": 0.2,
-        "RESEST_UPLINE": 0.0,
+        "RESET_UPLINE": 0.0,
         "STACK_IN_CUT": 0.5,
         "STACK_BREAK_CUT": 0.2,
         "STACK_DEEP_CUT": 0.1,
-        "HAIL_MARY": 0.0,
     },
 
     "STACK_DEEP_CUT": {
         "START": 0.0,
         "RESET_BREAK": 0.5,
-        "RESEST_UPLINE": 0.3,
+        "RESET_UPLINE": 0.3,
         "STACK_IN_CUT": 0.1,
         "STACK_BREAK_CUT": 0.1,
         "STACK_DEEP_CUT": 0.0,
-        "HAIL_MARY": 0.0,
-    },
-
-    "HAIL_MARY": {
-        "START": 0.0,
-        "RESET_BREAK": 1.0,
-        "RESEST_UPLINE": 0.0,
-        "STACK_IN_CUT": 0.0,
-        "STACK_BREAK_CUT": 0.0,
-        "STACK_DEEP_CUT": 0.0,
-        "HAIL_MARY": 0.0,
     },
 
 
@@ -96,7 +80,7 @@ TRANSITION_MATRIX = {
 #     "START": {
 #         "START": 0.0,
 #         "RESET_BREAK": 0.25,
-#         "RESEST_UPLINE": 0.15,
+#         "RESET_UPLINE": 0.15,
 #         "STACK_IN_CUT": 0.45,
 #         "STACK_BREAK_CUT": 0.05,
 #         "STACK_DEEP_CUT": 0.05,
@@ -106,17 +90,17 @@ TRANSITION_MATRIX = {
 #     "RESET_BREAK": {
 #         "START": 0.0,
 #         "RESET_BREAK": 0.0,
-#         "RESEST_UPLINE": 0.0,
+#         "RESET_UPLINE": 0.0,
 #         "STACK_IN_CUT": 0.5,
 #         "STACK_BREAK_CUT": 0.3,
 #         "STACK_DEEP_CUT": 0.1,
 #         "HAIL_MARY": 0.1,
 #     },
 
-#     "RESEST_UPLINE": {
+#     "RESET_UPLINE": {
 #         "START": 0.0,
 #         "RESET_BREAK": 0.0,
-#         "RESEST_UPLINE": 0.0,
+#         "RESET_UPLINE": 0.0,
 #         "STACK_IN_CUT": 0.1,
 #         "STACK_BREAK_CUT": 0.0,
 #         "STACK_DEEP_CUT": 0.7,
@@ -126,7 +110,7 @@ TRANSITION_MATRIX = {
 #     "STACK_IN_CUT": {
 #         "START": 0.0,
 #         "RESET_BREAK": 0.4,
-#         "RESEST_UPLINE": 0.2,
+#         "RESET_UPLINE": 0.2,
 #         "STACK_IN_CUT": 0.2,
 #         "STACK_BREAK_CUT": 0.1,
 #         "STACK_DEEP_CUT": 0.1,
@@ -136,7 +120,7 @@ TRANSITION_MATRIX = {
 #     "STACK_BREAK_CUT": {
 #         "START": 0.0,
 #         "RESET_BREAK": 0.2,
-#         "RESEST_UPLINE": 0.0,
+#         "RESET_UPLINE": 0.0,
 #         "STACK_IN_CUT": 0.5,
 #         "STACK_BREAK_CUT": 0.2,
 #         "STACK_DEEP_CUT": 0.1,
@@ -146,7 +130,7 @@ TRANSITION_MATRIX = {
 #     "STACK_DEEP_CUT": {
 #         "START": 0.0,
 #         "RESET_BREAK": 0.5,
-#         "RESEST_UPLINE": 0.3,
+#         "RESET_UPLINE": 0.3,
 #         "STACK_IN_CUT": 0.1,
 #         "STACK_BREAK_CUT": 0.1,
 #         "STACK_DEEP_CUT": 0.0,
@@ -156,7 +140,7 @@ TRANSITION_MATRIX = {
 #     "HAIL_MARY": {
 #         "START": 0.0,
 #         "RESET_BREAK": 1.0,
-#         "RESEST_UPLINE": 0.0,
+#         "RESET_UPLINE": 0.0,
 #         "STACK_IN_CUT": 0.0,
 #         "STACK_BREAK_CUT": 0.0,
 #         "STACK_DEEP_CUT": 0.0,
@@ -202,13 +186,13 @@ class PlayMaker:
         # Draw the field
     def draw_field(self, screen):
         screen.fill(GREEN)
-        pygame.draw.rect(screen, WHITE, (FIELD_WIDTH, 0, SCREEN_WIDTH-FIELD_WIDTH, SCREEN_HIGHT))  # Top scoring zone
+        pygame.draw.rect(screen, WHITE, (FIELD_WIDTH, 0, SCREEN_WIDTH-FIELD_WIDTH, SCREEN_HEIGHT))  # Right stats zone
 
                                         # (x, y, width, height)
         pygame.draw.rect(screen, WHITE, (0, 0, FIELD_WIDTH, SCORING_ZONE_HEIGHT))  # Top scoring zone
         pygame.draw.rect(screen, WHITE, (0, FIELD_HEIGHT - SCORING_ZONE_HEIGHT, FIELD_WIDTH, SCORING_ZONE_HEIGHT))  # Bottom scoring zone
 
-        pygame.draw.line(screen, BLACK, (SCREEN_WIDTH - FIELD_WIDTH, 0), (SCREEN_WIDTH - FIELD_WIDTH, SCREEN_HIGHT), 3)
+        pygame.draw.line(screen, BLACK, (SCREEN_WIDTH - FIELD_WIDTH, 0), (SCREEN_WIDTH - FIELD_WIDTH, SCREEN_HEIGHT), 3)
 
     def move_all_players_detla(self, right=0, up=0, exclude_index=None):
 
@@ -228,27 +212,31 @@ class PlayMaker:
     def update_disc_pos(self):
         self.disc_pos = [self.players[self.holder][0], self.players[self.holder][1]]
 
+    def check_point_scored(self):
+        # print("POINT")
+        return self.disc_pos[1] <= SCORING_ZONE_HEIGHT
+
 
     def return_to_position(self, index):
-        if index == 0:
+        if index == 0:  # Secondarily handler
             handler_pos = self.get_player_pos(1)
             self.move_player(index, handler_pos[0]-HANDLER_OFFSET ,handler_pos[1])
         
-        elif index == 1:
+        elif index == 1:  # Primarily handler
             handler_pos = self.get_player_pos(0)
             self.move_player(index, handler_pos[0]+HANDLER_OFFSET, handler_pos[1])
        
-        elif index == 2:
+        elif index == 2:  # First in stack
             stack_mid_pos = self.get_player_pos(3)
             self.move_player(index, stack_mid_pos[0], stack_mid_pos[1] + STACK_SPACING)
 
-        elif index == 3:
+        elif index == 3:  # Second in stack
             front_stack_pos = self.get_player_pos(2)
             self.move_player(index, front_stack_pos[0], front_stack_pos[1] - STACK_SPACING)
 
             self.last_in_stack += 1
 
-        elif index == 4:
+        elif index == 4:  # Third in stack
             stack_mid_pos = self.get_player_pos(2)
             self.move_player(index, stack_mid_pos[0], stack_mid_pos[1] - STACK_SPACING*2)
             self.last_in_stack += 1
@@ -278,20 +266,21 @@ class PlayMaker:
             self.pass_to_player_and_clear(0)
 
     def make_in_cut(self):
+        current_pos = self.get_player_pos(self.last_in_stack)
+        if self.last_in_stack == 4:
+            self.move_player(self.last_in_stack, current_pos[0] + 100, current_pos[1] + 100) # Move a bit down and right
+        elif self.last_in_stack == 3:
+            self.move_player(self.last_in_stack, current_pos[0] + 100, current_pos[1]) # Just move right
+        else:
+            raise ValueError(f'Too many cuts: {self.last_in_stack}')
+
+    def make_in_cut_break(self):
         current_pos = self.get_player_pos(len(self.players) - 1)
-        self.move_player(len(self.players) -1, current_pos[0] + 100, current_pos[1] + 100)
-        # print("\n\nLast in stack in in cut before removing: ", self.last_in_stack)
-        # self.last_in_stack -= 1
+        self.move_player(len(self.players) -1, current_pos[0] - 100, current_pos[1] + 100)
+
 
     def make_deep_cut(self):
-        # last_in_stack = len(self.players) - 1
-
-        # if last_in_stack == self.holder:
-        #     last_in_stack -= 1
-        
-        # print("Last in stack in deep cut: ", self.last_in_stack)
         self.move_player(self.last_in_stack, FIELD_WIDTH * 0.75, SCORING_ZONE_HEIGHT/2)
-        # self.last_in_stack -= 1
 
     
 
@@ -300,131 +289,110 @@ class PlayMaker:
         self.last_in_stack -= 1
 
     
-    def pass_to_deep_cut(self):
-        self.pass_to_player_and_clear(self.last_in_stack)
-        self.last_in_stack -= 1
-
-    
-    
     def do_next_move(self, next_move):
         if next_move == "RESET_BREAK":
             self.pass_to_reset()
-        
-        elif next_move == "RESEST_UPLINE":
+        elif next_move == "RESET_UPLINE":
             self.pass_to_reset()
         elif next_move == "STACK_IN_CUT":
-            self.pass_to_cut()
+            self.make_in_cut()
         elif next_move == "STACK_BREAK_CUT":
-            pass
+            self.make_in_cut_break()
         elif next_move == "STACK_DEEP_CUT":
-            pass
-        elif next_move == "HAIL_MARY":
-            pass
-
+            self.make_deep_cut()
+        else:
+            raise ValueError(f'Invalid move: {next_move}')
 
 
 def main():
     playmaker = PlayMaker(players=players_starting)
 
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("PlayMaker Simulation")
     clock = pygame.time.Clock()
 
-
+    playmaker.draw_field(screen)
+    playmaker.draw_players(screen)
+    
     running = True
     current_move = "START"
-    font = pygame.font.Font(None, 36)
-
-    playmaker.draw_field(screen)
-    # playmaker.draw_players(screen)
-
-    # playmaker.move_all_players(0, up=100)
-
-    # playmaker.draw_players(screen)
-
-    # pygame.display.flip()
-    # # pygame.display.update()
-
-    # # clock.tick(1)  # Update once per second
-
-    # playmaker.draw_field(screen)
-    # playmaker.draw_players(screen)
-    # pygame.display.flip()
-
-    # playmaker.move_all_players_detla(0, up=20, exclude_index=playmaker.holder)
-
-
-    # pygame.display.flip()
-
-    # if playmaker.holder != 4:
-    #     playmaker.pass_to_deep_cut()
-
     i = 0
+    pass_next = False
+    move_after_cut = False
 
     while running:
+        i += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # next_move = playmaker.get_next_move(current_move)
-        # text = font.render(f"Current Move: {current_move}", True, (0, 0, 0))
-        # screen.blit(text, (50, 50))
-        # text = font.render(f"Next Move: {next_move}", True, (0, 0, 0))
-        # screen.blit(text, (50, 100))
+        if i == 3:
+            playmaker.do_next_move("STACK_IN_CUT")
+        
+        if i == 5:
+            playmaker.pass_to_cut()
+        
+        if i == 7:
+            playmaker.move_all_after_cut()
 
-        pygame.display.flip()
-        # pygame.display.update()
-
-        clock.tick(1)  # Update once per second
+        if i == 9:
+            playmaker.do_next_move("RESET_BREAK")
 
         playmaker.draw_field(screen)
         playmaker.draw_players(screen)
 
-        # playmaker.move_all_players_detla(0, up=20, exclude_index=playmaker.holder)
-        if i <= 2:
-            playmaker.move_all_players_detla(0, up=20, exclude_index=playmaker.holder)
+        pygame.display.flip()
+
+        clock.tick(1)
+
+
+
+
+    # while running:
+
+    #     i += 1
+    #     print(f'\n\nIteration: {i}, Current Move: {current_move}')
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+            
+    #     if move_after_cut:
+    #         print("Adjusting after cut")
+    #         playmaker.move_all_after_cut()
+    #         move_after_cut = False
+            
+
+    #     if pass_next:
+    #         print("Passing to cut")
+    #         playmaker.pass_to_cut()
+    #         pass_next = False
+    #         move_after_cut = True
+
+    #     elif "CUT" in current_move: 
+    #         print(f'Cut move: {current_move}')
+    #         pass_next = True
+            
+
+
+    #     else:
+    #         playmaker.do_next_move(current_move)
+    #         current_move = playmaker.get_next_move(current_move)
+
+    #     playmaker.draw_field(screen)
+    #     playmaker.draw_players(screen)
+
+    #     pygame.display.flip()
+
+    #     if playmaker.check_point_scored():
+    #         running = False
         
-        if i == 3:
-            playmaker.do_next_move("RESET_BREAK")
+    #     # clock.tick(1)  # Update once per second
+    #     pygame.time.wait(5000)
 
-        # if i == 4:
-            # playmaker.move_player(3, 100,  100)
 
-        if i == 5 or i == 6:
-            playmaker.move_all_players_detla(0, up=20, exclude_index=playmaker.holder)
-
-        # if i == 7:
-        #     playmaker.return_to_position(3)
-
-        if i == 8:
-            playmaker.pass_to_reset()
-        
-        if i == 9:
-            playmaker.make_in_cut()
-
-        if i == 10:
-            playmaker.pass_to_cut()
-
-        if i == 11: 
-            playmaker.move_all_after_cut()
-        
-        # if i == 12:
-        #     playmaker.do_next_move("RESET_BREAK")
-
-        if i == 13:
-            playmaker.make_deep_cut()
-        
-        if i == 14:
-            playmaker.pass_to_cut()
-        # if playmaker.holder != 4:
-        #     playmaker.pass_to_deep_cut()
-
-        # current_move = next_move
-
-        i += 1
-
-    pygame.quit()
+    # print("Game Over")
+    # pygame.quit()
 
 
 main()
